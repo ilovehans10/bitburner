5dca7833483358d27a2894fba2636eb0a20688ca
Hans Larsson <hanandlia@gmail.com>
2025-09-07 12:33:52 -0500

    adjust threshold on sleeve.ts

--- sleeve.ts
+++ sleeve.ts
@@ -8,7 +8,7 @@ export async function main(ns: NS) {
   let loop_count = 0;
   const commiting_crimes = [false, false, false, false];
   const sleeve_timing = 30;
-  const sleeve_thresholds = { "shock": 80, "combat": 90 }
+  const sleeve_thresholds = { "shock": 85, "combat": 90 }
   const combat_skills = [ns.enums.GymType.strength, ns.enums.GymType.dexterity, ns.enums.GymType.defense, ns.enums.GymType.agility];
   do {
     if (ns.sleeve.getNumSleeves() > 0 && loop_count % sleeve_timing == 0) {
