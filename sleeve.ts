3ea66623299acc1080f1be757cae5aff73ce3cb9
Hans Larsson <hanandlia@gmail.com>
2025-09-06 20:13:02 -0500

    add support for sleeves to work on karma

--- sleeve.ts
+++ sleeve.ts
@@ -1,18 +1,27 @@
-import { NS } from "@ns";
+import { quiet_methods } from "library.js"
+import { NS, Skills } from "@ns";
 
 /** @param {NS} ns */
 export async function main(ns: NS) {
+  quiet_methods(ns, ["disableLog", "sleep", "singularity.upgradeHomeRam"]);
+
   let loop_count = 0;
+  const commiting_crimes = [false, false, false, false];
   const sleeve_timing = 30;
-  const sleeve_thresholds = { "shock": 30, "sync": 99 }
+  const sleeve_thresholds = { "shock": 80, "combat": 90 }
+  const combat_skills = [ns.enums.GymType.strength, ns.enums.GymType.dexterity, ns.enums.GymType.defense, ns.enums.GymType.agility];
   do {
     if (ns.sleeve.getNumSleeves() > 0 && loop_count % sleeve_timing == 0) {
       for (let sleeve_index = 0; sleeve_index < ns.sleeve.getNumSleeves(); sleeve_index++) {
         const sleeve = ns.sleeve.getSleeve(sleeve_index);
         if (sleeve.shock >= sleeve_thresholds.shock) {
           ns.sleeve.setToShockRecovery(sleeve_index);
-        } else if (sleeve.sync <= sleeve_thresholds.sync) {
-          ns.sleeve.setToSynchronize(sleeve_index);
+        } else if (get_sleeve_combat_average(sleeve.skills) < sleeve_thresholds.combat) {
+          const combat_choice = combat_skills[(loop_count / sleeve_timing) % 4];
+          ns.sleeve.setToGymWorkout(sleeve_index, "Powerhouse Gym", combat_choice);
+        } else if (!commiting_crimes[sleeve_index]) {
+          ns.sleeve.setToCommitCrime(sleeve_index, "Homicide");
+          commiting_crimes[sleeve_index] = true;
         }
       }
     }
@@ -20,4 +29,8 @@ export async function main(ns: NS) {
     await ns.sleep(1000)
     loop_count += 1;
   } while (true);
+}
+
+function get_sleeve_combat_average(skills: Skills) {
+  return (skills.agility + skills.defense + skills.dexterity + skills.strength) / 4
 }
