7c49e0d11f7f9ad6397737a6ad4596e89bb24736
Hans Larsson <hanandlia@gmail.com>
2025-09-12 15:30:21 -0500

    add encryption II solver

--- coding_contract.ts
+++ coding_contract.ts
@@ -24,6 +24,11 @@ export async function main(ns: NS) {
             solved = true;
             break;
 
+          case "Encryption II: Vigenère Cipher":
+            answer = solver_encryption_ii(contract_data[0], contract_data[1]);
+            solved = true;
+            break;
+
           default:
             ns.tprintf("Could not find solver for: %s on %s", contract_type, server)
             break;
@@ -78,4 +83,19 @@ function solver_largest_prime_factor(number: number) {
     }
   }
   return factors.reduce((a, b) => Math.max(a, b))
+}
+
+function solver_encryption_ii(plaintext: string, keyword: string): string {
+  const plaintext_array = plaintext.split("");
+  const keyword_array = keyword.split("");
+  const alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  let encrypted_string = "";
+  for (const character_index in plaintext_array) {
+    const plaintext_character = plaintext_array[character_index]
+    const keyword_character = keyword_array[Number(character_index) % keyword_array.length]
+    const plaintext_index = alphabet.indexOf(plaintext_character);
+    const keyword_index = alphabet.indexOf(keyword_character);
+    encrypted_string = encrypted_string.concat(alphabet[(plaintext_index + keyword_index) % alphabet.length])
+  }
+  return encrypted_string;
 }
