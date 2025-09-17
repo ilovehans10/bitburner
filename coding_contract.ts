ab42224e09e6150eb1ec03d0f77dabbaa59a2b83
Hans Larsson <hanandlia@gmail.com>
2025-09-16 21:01:14 -0500

    remove extra variable

--- coding_contract.ts
+++ coding_contract.ts
@@ -2,7 +2,6 @@ import { NS, AutocompleteData } from "@ns";
 import { connect_to_server, get_servers } from "library";
 
 const alphabet_upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-const alphabet_lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 export async function main(ns: NS) {
   const args = ns.args;
