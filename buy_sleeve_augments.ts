502d56b244e82466b95d9b4e6bdb10f42075db91
Hans Larsson <hanandlia@gmail.com>
2025-11-04 15:04:05 -0600

    add printout to sleeve augment purchases
    Signed-off-by: Hans Larsson <hanandlia@gmail.com>

--- buy_sleeve_augments.ts
+++ buy_sleeve_augments.ts
@@ -4,8 +4,14 @@ import { NS } from "@ns";
 export async function main(ns: NS) {
   const sleeve_indexes = (ns.args.length > 0) ? ns.args.map(a => Number(a)) : [...Array(ns.sleeve.getNumSleeves()).keys()];
   for (const index of sleeve_indexes) {
+    let money_spent = 0;
+    let augments_purchased = 0;
     for (const augment of ns.sleeve.getSleevePurchasableAugs(index)) {
-      ns.sleeve.purchaseSleeveAug(index, augment.name);
+      if (ns.sleeve.purchaseSleeveAug(index, augment.name)) {
+        money_spent += augment.cost;
+        augments_purchased += 1;
+      }
     }
+    ns.tprintf("There were %s augments purchased for sleeve %s costing $%s", augments_purchased.toString().padStart(3, "0"), index, money_spent);
   }
 }
