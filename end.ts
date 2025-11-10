157ef21e04bd6596e2b32fdf737d7df85476c2ab
Hans Larsson <hanandlia@gmail.com>
2025-11-10 09:16:14 -0600

    fix augmentation buying in end.ts
    Signed-off-by: Hans Larsson <hanandlia@gmail.com>

--- end.ts
+++ end.ts
@@ -18,7 +18,7 @@ export async function main(ns: NS) {
   }
 
   const augments = ["Embedded Netburner Module Core V3 Upgrade", "PC Direct-Neural Interface NeuroNet Injector", "Embedded Netburner Module Direct Memory Access Upgrade", "Embedded Netburner Module Analyze Engine", "Unstable Circadian Modulator", "SPTN-97 Gene Modification", "PC Direct-Neural Interface Optimization Submodule", "Embedded Netburner Module Core V2 Upgrade", "BitRunners Neurolink", "Xanipher", "PC Direct-Neural Interface", "Neuralstimulator", "Artificial Bio-neural Network Implant", "HyperSight Corneal Implant", "SmartJaw", "Embedded Netburner Module Core Implant", "Cranial Signal Processors - Gen V", "PCMatrix", "Neural Accelerator", "Enhanced Myelin Sheathing", "Enhanced Social Interaction Implant", "Cranial Signal Processors - Gen IV", "FocusWire", "Cranial Signal Processors - Gen III", "The Black Hand", "Neuroreceptor Management Implant", "ADR-V2 Pheromone Gene", "DataJack", "The Shadow's Simulacrum", "Neuregen Gene Modification", "Neural-Retention Enhancement", "Embedded Netburner Module", "CRTX42-AA Gene Modification", "TITN-41 Gene-Modification Injection", "Power Recirculation Core", "Neurotrainer III", "Cranial Signal Processors - Gen II", "CashRoot Starter Kit", "Artificial Synaptic Potentiation", "Cranial Signal Processors - Gen I", "Speech Processor Implant", "Neurotrainer II", "Nuoptimal Nootropic Injector Implant", "ADR-V1 Pheromone Gene", "Speech Enhancement", "BitWire", "Synaptic Enhancement Implant", "Neurotrainer I", "The Red Pill"];
-  const factions = ["NiteSec", "CyberSec", "BitRunners", "The Black Hand", "Daedalus"];
+  const factions = ns.getPlayer().factions;
 
   const extra_augments = [];
   for (const augment of user_augments) {
@@ -35,19 +35,19 @@ export async function main(ns: NS) {
 
   const all_factions = [ns.gang.getGangInformation().faction].concat(factions);
 
-  for (const faction of all_factions) {
-    for (let i = 0; i < 4; i++) {
-      for (const augment of augments) {
-        if (ns.singularity.purchaseAugmentation(faction as FactionName, augment)) {
-          ns.tprintf("%s: %s,", ns.singularity.getAugmentationBasePrice(augment), augment);
-        }
+  for (const augment of augments) {
+    for (const faction of all_factions) {
+      if (ns.singularity.purchaseAugmentation(faction as FactionName, augment)) {
+        ns.tprintf("%s: %s,", ns.singularity.getAugmentationBasePrice(augment), augment);
       }
-      let governor_bought;
-      do {
-        const governor_name = "NeuroFlux Governor";
-        governor_bought = ns.singularity.purchaseAugmentation(faction as FactionName, governor_name);
-      } while (governor_bought);
     }
+  }
+  for (const faction of all_factions) {
+    let governor_bought;
+    do {
+      const governor_name = "NeuroFlux Governor";
+      governor_bought = ns.singularity.purchaseAugmentation(faction as FactionName, governor_name);
+    } while (governor_bought);
   }
   ns.singularity.installAugmentations("callback.js");
 }
